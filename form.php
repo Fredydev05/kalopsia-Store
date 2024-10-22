6ce54828780cf1d2dd817fb7541692814ade9a90
Lamaraaash <cespedesmatias123@gmail.com>
2024-10-22 11:02:11 -0300

    cambios nuevamente

--- form.php
+++ form.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 if(isset($_POST['enviar'])){
@@ -8,15 +7,4 @@ if(isset($_POST['enviar'])){
         $email = $_POST['email'];
         $message = $_POST['message'];
     }
-=======
-<?php
-
-if(isset($_POST['enviar'])){
-    if (!empty($_POST['nombre']) && !empty($_POST['phone']) && !empty($_POST['email']) && !empty($_POST['message'])) {
-        $name = $_POST['nombre'];
-        $phone = $_POST['phone'];
-        $email = $_POST['email'];
-        $message = $_POST['message'];
-    }
->>>>>>> 0197b46 (New updates and add translate options)
 }
