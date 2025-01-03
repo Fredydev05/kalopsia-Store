3341f1ec3468a723bcfab46516e900565af7a823
Fredydev05 <cespedesmatias123@gmail.com>
2025-01-03 13:07:46 -0300

    add info products images in a new file

---
+++ JS/productos.js
@@ -0,0 +1,43 @@
+export const contenido = {
+
+    jarroncitos: {
+      titulo: "Jarroncitos",
+      descripcion: "Información sobre jarroncitos...",
+      subdesc: "(click en la imagen para zoom)",
+      imagenes: [
+        "./img/productos/jarroncitos/jarrito gibili.jpg",
+        "./img/productos/jarroncitos/jarroncito 2-1.jpg",
+        "./img/productos/jarroncitos/jarroncito 2.jpg",
+        "./img/productos/jarroncitos/jarroncito blanco.jpg",
+        "./img/productos/jarroncitos/jarroncito rosa.jpg",
+        "./img/productos/jarroncitos/jarroncito gibili 2.jpg",
+      ],
+    },
+
+    tazas: {
+      titulo: "Tazas",
+      descripcion: "Información sobre tazas...",
+      subdesc: "(click en la imagen para zoom)",
+      imagenes: [
+        "./img/productos/tazas/taza 1.jpg",
+        "./img/productos/tazas/taza kanye 2.jpg",
+        "./img/productos/tazas/taza kanye.jpg",
+        "./img/productos/tazas/taza slipknot.jpg",
+        "./img/productos/tazas/taza suga 2.jpg",
+        "./img/productos/tazas/taza tylor 2.jpg",
+        "./img/productos/tazas/taza tylor.jpg",
+      ],
+    },
+
+    platos: {
+      titulo: "Platos",
+      descripcion: "Información sobre platos...",
+      subdesc: "(click en la imagen para zoom)",
+      imagenes: [
+        "./img/productos/platos/plato1.jpg",
+        "./img/productos/platos/plato2.jpg",
+        "./img/productos/platos/plato3.jpg",
+      ],
+
+    },
+  }; 
